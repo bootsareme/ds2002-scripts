d9a9fe42c5e4600065f05ea925028e9792f7ddf7
Vincent Zhang <73851560+bootsareme@users.noreply.github.com>
2025-02-05 21:48:56 +0000

    Classwork for feb 5 2025

---
+++ fileinfo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+filename=$1
+
+if [ ! -e "$filename" ]; then
+  echo "Error: File '$filename' not found."
+  exit 1
+fi
+
+filesize=$(ls -lh "$filename" | awk '{print $5}')
+filetype=$(file "$filename" | awk -F: '{print $2}' | xargs)  # xargs removes extra whitespace
+lastmod=$(date -r "$filename" "+%Y-%m-%d %H:%M:%S")
+# add another var in this group that displays how many lines the file contains.
+lines=$(cat "$filename" | wc -l)
+# echo out those fields:values each on its own line
+echo "file name:" $filename
+echo "file size:" $filesize
+echo "last modification:" $lastmod
+echo "lines:" $lines
+exit 0
